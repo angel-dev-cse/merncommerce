d1cbd76890238706afc8c36bd97c9a739d687ccf
Angel Sharma <angel.dev.cse@gmail.com>
2024-07-16 13:10:17 +0600

    Added update product + fix

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const {
   createProduct,
   getProduct,
@@ -7,9 +8,9 @@ const {
   updateProduct,
 } = require("../controller/productController");
 
-router.post("/create", createProduct);
-router.get("/:id", getProduct);
-router.get("/", getProducts);
-router.put("/:id", updateProduct);
+router.post("/create", authMiddleware, isAdmin, createProduct);
+router.put("/:id", authMiddleware, isAdmin, updateProduct);
+router.get("/:id", authMiddleware, getProduct);
+router.get("/", authMiddleware, getProducts);
 
 module.exports = router;
