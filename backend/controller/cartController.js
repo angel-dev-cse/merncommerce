98e2c4c7c3bf5664e9a06e873b2e9165ebd0d18d
Angel Sharma <angel.dev.cse@gmail.com>
2024-08-13 18:04:16 +0600

    Added removing product from cart

--- backend/controller/cartController.js
+++ backend/controller/cartController.js
@@ -4,6 +4,11 @@ const asyncHandler = require("express-async-handler");
 const validateMongoID = require("../validations/validateMongoID");
 const { cartSchema } = require("../validations/validationSchema");
 
+// @desc    Add product to cart or update quantity
+// @route   POST /api/cart
+// @access  Private
+// @role    User
+// @validation  id, quantity, color
 const addToCart = asyncHandler(async (req, res) => {
   const { error } = cartSchema.validate(req.body);
   if (error) {
@@ -51,6 +56,10 @@ const addToCart = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc   Get cart
+// @rotue GET /api/cart
+// @access Private
+// @role   User
 const getCart = asyncHandler(async (req, res) => {
   const { _id } = req.user;
   validateMongoID(_id);
@@ -61,4 +70,34 @@ const getCart = asyncHandler(async (req, res) => {
   res.status(200).json(cart);
 });
 
-module.exports = { addToCart, getCart };
+// @desc   Remove product from cart
+// @route  DELETE /api/cart/:id
+// @access Private
+// @role   User
+// @validation id, _id
+const removeFromCart = asyncHandler(async (req, res) => {
+  // _id is user id
+  const { _id } = req.user;
+  validateMongoID(_id);
+  // id is product id
+  const { id } = req.params;
+  validateMongoID(id);
+
+  const cart = await Cart.findOne({ user: _id });
+  if (!cart) throw new Error("Cart not found!");
+
+  const productIndex = cart.products.findIndex(
+    (p) => p.id.toString() === id.toString()
+  );
+  if (productIndex === -1) throw new Error("Product not found in cart!");
+
+  cart.products.splice(productIndex, 1);
+  cart.totalPrice = cart.products.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+  await cart.save();
+  res.json({ message: "Product removed from cart!", cart: cart });
+});
+
+module.exports = { addToCart, getCart, removeFromCart };
--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { addToCart, getCart } = require("../controller/cartController");
+const { addToCart, getCart, removeFromCart } = require("../controller/cartController");
 const { authMiddleware } = require("../middlewares/authMiddleware");
 
 router.post("/add", authMiddleware, addToCart);
 router.get("/", authMiddleware, getCart);
+router.delete("/:id", authMiddleware, removeFromCart);
 
 module.exports = router;
